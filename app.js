3b1b4b85e10a7c0c8075033c163710c2793c4f06
anarod13 <anam.rodriguez13@gmail.com>
2022-09-20 00:35:19 -0300

    Create endpoints

---
+++ app.js
@@ -0,0 +1,83 @@
+import express from "express";
+import multer from "multer";
+import * as fs from "fs";
+import * as url from "url";
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+const app = express();
+const puerto = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const data = fs.readFileSync("src/data/teams.json");
+const listedTeams = JSON.parse(data);
+
+app.get("/", (req, res) => {
+  try {
+    res.end(JSON.stringify(listedTeams));
+  } catch (err) {
+    console.error(err);
+    return;
+  }
+});
+
+app.get("/:team", (req, res) => {
+  try {
+    const team = listedTeams.find(
+      (team) => team.id === Number(req.params.team)
+    );
+    res.end(JSON.stringify(team));
+  } catch (err) {
+    console.error(err);
+    return;
+  }
+});
+
+app.patch("/:team/edit", (req, res) => {
+  try {
+    const editedTeamId = Number(req.params.team);
+    const editedTeam = req.body;
+    editedTeam.lastUpdated = new Date();
+    const editedTeamIndex = listedTeams.findIndex(
+      (team) => team.id === editedTeamId
+    );
+    listedTeams[editedTeamIndex] = editedTeam;
+    fs.writeFileSync("src/data/teams.json", JSON.stringify(listedTeams));
+    res.end();
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.put("/add", (req, res) => {
+  try {
+    const newTeam = req.body;
+
+    const newTeamId = listedTeams.length;
+    newTeam.id = newTeamId;
+    newTeam.lastUpdated = new Date();
+    listedTeams.push(newTeam);
+    fs.writeFileSync("src/data/teams.json", JSON.stringify(listedTeams));
+    res.end();
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.delete("/:team/delete", (req, res) => {
+  try {
+    const team = listedTeams.find(
+      (team) => team.id === Number(req.params.team)
+    );
+    listedTeams.splice(listedTeams.indexOf(team), 1);
+    fs.writeFileSync("src/data/teams.json", JSON.stringify(listedTeams));
+    res.end();
+  } catch (e) {
+    console.log(e);
+    3;
+  }
+});
+
+app.listen(puerto);
