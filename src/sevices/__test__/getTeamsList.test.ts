1c0758cc0f91c4fb22b225676a71665dfa8aa5d3
anarod13 <anam.rodriguez13@gmail.com>
2022-10-08 14:56:41 -0300

    Add get teams test

---
+++ src/sevices/__test__/getTeamsList.test.ts
@@ -0,0 +1,24 @@
+import mockTeamData from "../../helpers/__tests__/fixtures/mockTeamData.json";
+import fs from "fs";
+import { getTeamsList } from "../crudClub";
+
+jest.mock("fs");
+const mockFS: jest.Mocked<typeof fs> = <jest.Mocked<typeof fs>>fs;
+const teamsListResult = [
+  { id: 57, name: "Arsenal FC", country: "England" },
+  { id: 58, name: "Aston Villa FC", country: "England" },
+  { id: 61, name: "Chelsea FC", country: "England" },
+  { id: 62, name: "Everton FC", country: "England" },
+];
+
+beforeAll(() => {
+  mockFS.readFileSync.mockClear();
+  mockFS.readFileSync.mockReturnValue(
+    Buffer.from(JSON.stringify(mockTeamData))
+  );
+});
+
+it("Gets a teams list", () => {
+  const teamsList = getTeamsList();
+  expect(teamsList).toEqual(teamsListResult);
+});
