7fe175dd17d7168b141fde87b688c8facebdd59b
anarod13 <anam.rodriguez13@gmail.com>
2022-10-27 23:14:26 -0300

    Refactor database helpers

--- src/helpers/dataBaseHelper.ts
+++ src/helpers/dataBaseHelper.ts
@@ -1,27 +1,45 @@
 import * as fs from "fs";
-import ITeam from "../data/ITeam";
+import IListedTeam from "../entities/IListedTeam";
+import ITeam from "../entities/IListedTeam";
+import ITeamToUpdate from "../entities/ITeamToUpdate";
 
-const DATA_SOURCE = "src/data/teams.json";
+const LISTED_TEAMS_PATH = "./src/data/teams.json";
+const TEAMS_DETAILS_PATH = "./src/data/teams/";
+const TEAMS_CRESTS_PATH = "./src/data/teams/";
 
-export function getDataBase(): ITeam[] {
-  const teamData = fs.readFileSync(DATA_SOURCE);
-  const listedTeams = JSON.parse(teamData.toString());
-  return listedTeams;
+export function getListedTeams(): IListedTeam[] {
+  const listedTeams = fs.readFileSync(LISTED_TEAMS_PATH);
+  return JSON.parse(listedTeams.toString());
 }
 
-export function updateDataBase(updateTeamList: ITeam[]) {
-  fs.writeFileSync(DATA_SOURCE, JSON.stringify(updateTeamList));
+export function updateListedTeams(updatedListedTeams: IListedTeam[]) {
+  fs.writeFileSync(LISTED_TEAMS_PATH, JSON.stringify(updatedListedTeams));
 }
 
-export function checkIfFileExists(filePath: string) {
+export function getTeamData(teamTLA: string): ITeam {
+  const teamFilePath = `${TEAMS_DETAILS_PATH}${teamTLA}.json`;
+  const teamData = fs.readFileSync(teamFilePath);
+  return JSON.parse(teamData.toString());
+}
+
+export function updateTeamData(teamToUpdate: ITeamToUpdate) {
+  const teamFilePath = `${TEAMS_DETAILS_PATH}${teamToUpdate.tla}.json`;
+  fs.writeFileSync(teamFilePath, JSON.stringify(teamToUpdate.teamData));
+}
+export function deleteTeamData(teamTLA: string) {
+  const teamFilePath = `${TEAMS_DETAILS_PATH}${teamTLA}.json`;
+  deleteFile(teamFilePath);
+}
+export function deleteTeamCrest(teamCrest: string) {
+  const teamCrestPath = `${TEAMS_CRESTS_PATH}${teamCrest}.png`;
+  deleteFile(teamCrestPath);
+}
+
+function deleteFile(filePath: string) {
   try {
     fs.statSync(filePath);
   } catch (e) {
     return false;
   }
-  return true;
-}
-
-export function deleteFile(filePath: string) {
   fs.unlinkSync(filePath);
 }
