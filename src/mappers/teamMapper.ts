f6c1dedf5ac49fc8a3be763d03f6561a704747cb
anarod13 <anam.rodriguez13@gmail.com>
2022-10-27 21:28:47 -0300

    Refactor listed team model

--- src/mappers/teamMapper.ts
+++
@@ -1,6 +0,0 @@
-import type ITeam from "../data/ITeam";
-import TeamListed from "./TeamListed";
-
-export function teamListedMapper(team: ITeam): TeamListed {
-  return new TeamListed(team.id, team.name, team.area.name);
-}
