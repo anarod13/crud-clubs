cd388a844565b378def44a7864e56a162435509b
anarod13 <anam.rodriguez13@gmail.com>
2022-11-19 14:21:24 -0300

    Add create new team test

---
+++ src/helpers/__tests__/createNewTeam.test.ts
@@ -0,0 +1,21 @@
+import { vi, Mocked, beforeAll, it, expect } from "vitest";
+import fs from "fs";
+import mockTeam from "./fixtures/mockTeam.json";
+import { createNewTeam } from "../dataBaseHelper";
+
+vi.mock("fs");
+const mockFS: Mocked<typeof fs> = <Mocked<typeof fs>>fs;
+const mockTeamTla = "mockTeam1";
+
+beforeAll(() => {
+  mockFS.writeFileSync.mockClear();
+  mockFS.writeFileSync.mockReturnValue();
+});
+it("Creates new team", () => {
+  createNewTeam(mockTeamTla, mockTeam);
+  expect(fs.writeFileSync).toHaveBeenCalledTimes(1);
+  expect(fs.writeFileSync).toHaveBeenCalledWith(
+    `./src/data/teams/${mockTeamTla}.json`,
+    JSON.stringify(mockTeam)
+  );
+});
